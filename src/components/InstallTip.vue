40a8c911b81aa129fd2396ba7ff00be1b8242756
ANJI-ALLWAYS\cuizhiliang <735896273@qq.com>
2021-01-20 17:54:41 +0800

    init

---
+++ src/components/InstallTip.vue
@@ -0,0 +1,50 @@
+<template>
+  <el-card class="box-card">
+    <div slot="header" class="clearfix">
+      <span>部署说明</span>
+      <el-button style="float: right; padding: 3px 0" type="text">
+        <a href="http://www.baidu.com" target="_blank">代码地址</a>
+      </el-button>
+    </div>
+    <div class="text item">
+      1. 前端安装依赖包 npm install
+      <br>
+      2. 前端启动 npm run serve <strong>注意:(调用后端修改 plugins中的axios.js 中的 baseURL配置)</strong>
+      <br>
+      3. 后端要注意跨域问题
+    </div>
+  </el-card>
+
+</template>
+
+<script>
+  export default {
+    name: "InstallTip"
+  }
+</script>
+
+<style scoped>
+  .text {
+    font-size: 14px;
+  }
+
+  .item {
+    margin-bottom: 18px;
+  }
+
+  .clearfix:before,
+  .clearfix:after {
+    display: table;
+    content: "";
+  }
+  .clearfix:after {
+    clear: both
+  }
+
+  .box-card {
+    width: 300px;
+    position: fixed;
+    top: 50px;
+    background-color: cyan;
+  }
+</style>
---
+++ src/views/Index.vue
@@ -0,0 +1,52 @@
+<template>
+  <el-container>
+    <el-header>gin gorm 框架小练习</el-header>
+    <el-main>
+      <el-row type="flex" justify="center">
+        <el-col :xs="20" :span="12" >
+          <div class="grid-content">
+            <el-divider>
+              <h1>TODO 清单</h1>
+            </el-divider>
+            <TodoList></TodoList>
+          </div>
+        </el-col>
+      </el-row>
+    </el-main>
+    <install-tip> </install-tip>
+    <el-footer>代码原始来源七米: <a href="https://github.com/Q1mi/bubble_frontend">七米源码</a></el-footer>
+  </el-container>
+
+
+</template>
+
+<script>
+// @ is an alias to /src
+import TodoList from "@/components/TodoList.vue";
+import InstallTip from "../components/InstallTip";
+
+export default {
+  name: "Index",
+  components: {
+    TodoList,
+    InstallTip
+  }
+};
+</script>
+<style>
+.el-header,
+.el-footer {
+  background-color: #409eff;
+  color: #fff;
+  text-align: center;
+  line-height: 60px;
+}
+.el-footer {
+  background-color: #909399;
+  /*display: block;*/
+  /*width: 100%;*/
+  /*position: fixed;*/
+  /*bottom: 0px;*/
+
+}
+</style>
